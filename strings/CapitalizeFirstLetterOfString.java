f2eddbf6bb245b06bf463e821f7d7dac09671820
anushakanda <71995197+anushakanda@users.noreply.github.com>
2023-05-27 16:56:46 +0530

    Update CapitalizeFirstLetterOfString.java

--- strings/CapitalizeFirstLetterOfString.java
+++ strings/CapitalizeFirstLetterOfString.java
@@ -1,1 +1,29 @@
+public class CapitalizeFirstLetterOfString {
+    
+  public static void main(String[] args){
+      List<String> messages = Arrays.asList("hello", "hi", "readers!","welcome"); 
+      // Approach 1: Using custom defined function.
+      messages.stream().map(message -> capitalize(message)).forEach(message-> System.out.println(message));
+      //Using method reference the above code can be written as
+      messages.stream().map(CapitalizeFirstLetterOfString::capitalize).forEach(System.out::println);
+    
+     //Approach 2:Use capitalize method in StringUTils from Apache commons
+      messages.stream().map(StringUtils::capitalize).forEach(System.out::println);
+  }
+  
+  private static String capitalize(String str){
+    if(str == null ||str.isEmpty()){
+      return str;
+    }
+    
+    String firstCharacter = str.subString(0,1).toUpperCase();
+    String capitalizedString = firstCharacter + str.subString(1);
+    
+    return capitalizedString;
+  }
+}
 
+//Things to learn:
+//1. Type Inference
+//2. Method Reference
+//3. String inbuilt methods
